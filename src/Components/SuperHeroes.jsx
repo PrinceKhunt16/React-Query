7ab06d5de5f8f12ab4e2359c7ea41d44aeac34f3
Prince Khunt <princekhuntdatastructure@gmail.com>
2022-10-10 23:37:54 +0530

    Change hooks

--- src/Components/SuperHeroes.jsx
+++ src/Components/SuperHeroes.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
-import { useSuperHeroesData } from '../Hooks/useSuperHeroesData'
+import React, { useState } from 'react'
+import { useAddSuperHeroData, useSuperHeroesData } from '../Hooks/useSuperHeroesData'
 import { Link } from "react-router-dom"
 
 const SuperHeroes = () => {
+    const [name, setName] = useState('')
+    const [alterEgo, setAlterEgo] = useState('')
     const { isLoading, isFetching, isError, error, data, refetch } = useSuperHeroesData();
+
+    const { mutate } = useAddSuperHeroData();
+
+    const handleAddHero = () => {
+        const hero = {
+            name: name,
+            alterEgo: alterEgo
+        }
+
+        mutate(hero);
+        setName('');
+        setAlterEgo('');
+    }
 
     if (isLoading || isFetching) {
         return <h2>Loading</h2>
@@ -15,6 +30,19 @@ const SuperHeroes = () => {
 
     return (
         <div>
+            <div>
+                <input
+                    type='text'
+                    value={name}
+                    onChange={e => setName(e.target.value)}
+                />
+                <input
+                    type='text'
+                    value={alterEgo}
+                    onChange={e => setAlterEgo(e.target.value)}
+                />
+                <button onClick={handleAddHero}>Add Hero</button>
+            </div>
             {data?.data.map((hero) => {
                 return (
                     <div key={hero.id} className="superHeroesLink">
--- src/Hooks/useSuperHeroesData.jsx
+++ src/Hooks/useSuperHeroesData.jsx
@@ -1,13 +1,28 @@
-import { useQuery } from 'react-query'
+import { useQuery, useMutation, useQueryClient } from 'react-query'
 import axios from 'axios'
 
 const fetchSuperHeroes = () => {
     return axios.get('http://localhost:4000/superheroes')
 }
 
+const addSuperHero = (hero) => {
+    return axios.post('http://localhost:4000/superheroes', hero);
+}
+
 export const useSuperHeroesData = () => {
-    return useQuery(
-        'super-heroes',
-        fetchSuperHeroes
-    );
+    return useQuery('super-heroes',fetchSuperHeroes);
+}
+
+export const useAddSuperHeroData = () => {
+    const queryClient = useQueryClient()
+    return useMutation(addSuperHero, {
+        onSuccess: (data) => {
+            queryClient.setQueryData('super-heroes', (oldQueryData) => {
+                return {
+                    ...oldQueryData,
+                    data: [...oldQueryData.data, data.data]
+                }
+            })
+        }
+    });
 }
